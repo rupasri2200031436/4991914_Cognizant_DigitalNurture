e78157f05e868459fd84e50ccd8976f603cf94f2
rupasri2200031436 <111578493+rupasri2200031436@users.noreply.github.com>
2025-06-29 16:00:31 +0530

    Add files via upload

---
+++ 4991914_Cognizant_DeepSkilling/Week2/JUnit_Testing/Exercise1/junit_exercise1/src/main/java/com/junit_exercise1/App.java
@@ -0,0 +1,7 @@
+package com.junit_exercise1;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ 4991914_Cognizant_DeepSkilling/Week2/JUnit_Testing/Exercise2/junit_exercise2/src/main/java/com/junit/junit_exercise2/App.java
@@ -0,0 +1,13 @@
+package com.junit.junit_exercise2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
