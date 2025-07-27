34cb13f50b66c79a621516cb75e1078250a96d1b
Naga Rupa Sri <nagarupasrik@gmail.com>
2025-07-27 15:24:41 +0530

    Added Week6 from deepskilling

---
+++ 4991914_deepskilling/Week6/ReactJS-HOL2/studentapp/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h2>Welcome to the About page of Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default About;
